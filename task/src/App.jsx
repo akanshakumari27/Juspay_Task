87674f757347ba6ff41e4fffeb70c705f44066b3
akanshakumari27 <94097093+akanshakumari27@users.noreply.github.com>
2025-09-19 22:39:35 +0530

    Devided into 4 grids with responsiveness and created seprate folder and files for each grid component, and will design using SCSS

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -3,7 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-      
+    <div className='container'>
+      <div className='sidebar'>Sidebar1</div>
+      <div className='navbar'>Navbar</div>
+      <div className='main_content'>MainContent</div>
+      <div className='right_sidebar'>Sidebar2</div>
+      </div>
     </>
   )
 }
